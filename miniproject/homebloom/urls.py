3426567532263494abad2a65d11c05f78e1efab2
Gopika <gopikak75317@gmail.com>
2025-12-09 22:05:26 +0530

    created some html pages

--- miniproject/homebloom/urls.py
+++ miniproject/homebloom/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from homebloom import views
 urlpatterns = [
-    path('',views.home,name='home')
-    
+    path('',views.home,name='home'),
+    path('shop',views.shop,name='shop'),
+    path('category',views.category,name='category'),
 ]
--- miniproject/homebloom/views.py
+++ miniproject/homebloom/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 # Create your views here.
+def shop(request):
+    return render(request,'shop.html')
+def category(request):
+    return render(request,'category.html')
+def wishlist(request):
+    return render(request,'wishlist.html')
